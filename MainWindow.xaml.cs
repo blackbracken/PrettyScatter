4d6f66418e1e4f71e159467e6e38f748591fb61e
blackbracken <me@bracken.black>
2021-10-06 20:49:04 +0900

    highlight element if mouse is in scatter plot

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using PrettyScatter.Models;
 using PrettyScatter.Utils.Ext;
+using ScottPlot;
+using ScottPlot.Plottable;
 
 namespace PrettyScatter
 {
     public partial class MainWindow : Window
     {
-        private Log? log = null;
+        private readonly ScatterPlot _myScatterPlot;
+        private readonly ScatterPlot _highlightedPoint;
+        private int _lastHighlightedIndex;
+        private bool _mouseInScatterPlot;
+
+        private Log? _log;
 
         public MainWindow()
         {
@@ -38,30 +43,75 @@ namespace PrettyScatter
                 };
             }
 
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 4, 9, 16, 25 };
-            SamplePlot.Plot.AddScatter(dataX, dataY);
+            {
+                double[] dataX = { 1, 2, 3, 4, 5 };
+                double[] dataY = { 1, 4, 9, 16, 25 };
+
+                _highlightedPoint = SamplePlot.Plot.AddPoint(0, 0);
+                _highlightedPoint.Color = System.Drawing.Color.Red;
+                _highlightedPoint.MarkerSize = 10;
+                _highlightedPoint.MarkerShape = MarkerShape.openCircle;
+                _highlightedPoint.IsVisible = false;
+
+                _myScatterPlot = SamplePlot.Plot.AddScatterPoints(dataX, dataY);
+                SamplePlot.Plot.AddScatter(dataX, dataY);
+
+                SamplePlot.Refresh();
+            }
+        }
+
+        private void OnMouseHoverPoint(object sender, MouseEventArgs ev)
+        {
+            if (!_mouseInScatterPlot) return;
+
+            HighlightNearestPlot(sender, ev);
+        }
+
+        private void OnMouseGoInScatterPlot(object sender, MouseEventArgs ev)
+        {
+            _mouseInScatterPlot = true;
+
+            HighlightNearestPlot(sender, ev, true);
+        }
+
+        private void OnMouseGoOutScatterPlot(object sender, MouseEventArgs ev)
+        {
+            _mouseInScatterPlot = false;
+
+            _highlightedPoint.IsVisible = false;
             SamplePlot.Refresh();
+        }
+
+        private void HighlightNearestPlot(object sender, MouseEventArgs ev, bool updateForce = false)
+        {
+            (var mouseCoordX, var mouseCoordY) = SamplePlot.GetMouseCoordinates();
+            var xyRatio = SamplePlot.Plot.XAxis.Dims.PxPerUnit / SamplePlot.Plot.YAxis.Dims.PxPerUnit;
+            (var pointX, var pointY, var pointIndex) =
+                _myScatterPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
+
+            _highlightedPoint.Xs[0] = pointX;
+            _highlightedPoint.Ys[0] = pointY;
+            _highlightedPoint.IsVisible = true;
+
+            if (_lastHighlightedIndex != pointIndex || updateForce)
+            {
+                _lastHighlightedIndex = pointIndex;
+                SamplePlot.Refresh();
+            }
         }
 
         private async void OnDropFiles(string[] paths)
         {
-            double[] dataX = { 30, 50, 0, 640, 501 };
-            double[] dataY = { 0, 3, 0, 305, 20 };
+            _log = await Log.FromFile(paths[0]);
 
-            log = await Log.FromFile(paths[0]);
             {
-                LogList.ItemsSource = new ObservableCollection<LogListItem>(log.LogTextList.Select((elem, idx) =>
+                LogList.ItemsSource = new ObservableCollection<LogListItem>(_log.LogTextList.Select((elem, idx) =>
                     new LogListItem
                     {
                         Index = idx,
                         Content = elem,
                     }));
             }
-
-            SamplePlot.Plot.RemoveAt(0);
-            SamplePlot.Plot.AddScatter(dataX, dataY);
-            SamplePlot.Refresh();
         }
 
         public struct LogListItem
