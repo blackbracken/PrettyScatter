7551ddf95d0b0ea6464239c666dd1b86130ac421
blackbracken <me@bracken.black>
2021-10-06 18:37:20 +0900

    support drag & drop

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using PrettyScatter.Utils.Ext;
 
 namespace PrettyScatter
 {
@@ -8,8 +9,38 @@ namespace PrettyScatter
         {
             InitializeComponent();
 
+            {
+                RootGrid.AllowDrop = true;
+                RootGrid.PreviewDragOver += (_, ev) =>
+                {
+                    if (ev.IsGottenFile())
+                    {
+                        ev.Effects = ev.Effects = DragDropEffects.Copy;
+                    }
+
+                    ev.Handled = true;
+                };
+                RootGrid.PreviewDrop += (_, ev) =>
+                {
+                    if (ev.IsGottenFile())
+                    {
+                        OnDropFiles(ev.GetFilePathsUnsafe());
+                    }
+                };
+            }
+
             double[] dataX = new double[] { 1, 2, 3, 4, 5 };
             double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            SamplePlot.Plot.AddScatter(dataX, dataY);
+            SamplePlot.Refresh();
+        }
+
+        private void OnDropFiles(string[] paths)
+        {
+            double[] dataX = { 30, 50, 0, 640, 501 };
+            double[] dataY = { 0, 3, 0, 305, 20 };
+
+            SamplePlot.Plot.RemoveAt(0);
             SamplePlot.Plot.AddScatter(dataX, dataY);
             SamplePlot.Refresh();
         }
---
+++ Utils/Ext/DragEventArgsExt.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PrettyScatter.Utils.Ext
+{
+    public static class DragEventArgsExt
+    {
+        public static bool IsGottenFile(this DragEventArgs ev) => ev.Data.GetDataPresent(DataFormats.FileDrop);
+
+        public static string[] GetFilePathsUnsafe(this DragEventArgs ev) =>
+            (string[])ev.Data.GetData(DataFormats.FileDrop);
+    }
+}
